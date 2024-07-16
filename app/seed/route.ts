30c4cab82f8dfed17462ec16f1f8983e30bb7339
Daniel <byoung.mokk@gmail.com>
2024-07-17 01:18:17 +0900

    Modify: route.ts

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -19,7 +19,7 @@ async function seedUsers() {
     users.map(async (user) => {
       const hashedPassword = await bcrypt.hash(user.password, 10);
       return client.sql`
-        INSERT INTO users (id, name, email, password)s
+        INSERT INTO users (id, name, email, password)
         VALUES (${user.id}, ${user.name}, ${user.email}, ${hashedPassword})
         ON CONFLICT (id) DO NOTHING;
       `;
